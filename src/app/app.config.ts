ce4004d46077246be0f09b346b7615000be03557
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-15 17:49:24 +0200

    Add Scrool to Ancor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import {
   inject,
   provideAppInitializer,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// Importiere zusätzlich 'withInMemoryScrolling'
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideTranslateService, TranslateService } from '@ngx-translate/core';
@@ -13,7 +14,14 @@ import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      // Hier fügst du das Scrolling-Verhalten hinzu
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      }),
+    ),
     provideHttpClient(),
     provideTranslateService({
       lang: 'de',
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { Burger } from './burger/burger';
 import { Navbar } from './navbar/navbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [Navbar, Burger],
+  imports: [RouterLink, Navbar, Burger],
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
 })
