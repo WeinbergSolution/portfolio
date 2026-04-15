9327114bd8595aab8262f63b2e5f624d41083f97
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-15 16:54:10 +0200

    Respnsiv(786)

--- src/app/pages/landing-page/projects/projects.ts
+++ src/app/pages/landing-page/projects/projects.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-projects',
