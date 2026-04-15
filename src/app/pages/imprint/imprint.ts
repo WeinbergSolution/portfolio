09e6fa4ae46b2b7faffc374d1fac6deb16b0c778
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-15 20:50:41 +0200

    Add: Imprint, Add new Logo,

--- src/app/pages/imprint/imprint.ts
+++ src/app/pages/imprint/imprint.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Footer } from '../../layout/footer/footer';
+import { Header } from '../../layout/header/header';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  standalone: true,
+  imports: [TranslatePipe, Footer, Header],
   templateUrl: './imprint.html',
   styleUrls: ['./imprint.scss'],
 })
-export class Imprint {}
+export class Imprint {
+  private translate = inject(TranslateService);
+}
